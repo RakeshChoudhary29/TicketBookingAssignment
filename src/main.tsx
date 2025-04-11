edf91f5b4b1a65f26e0821db3976d19af5fccc00
RakeshChoudhary29 <rakeshtheone29@gmail.com>
2025-04-11 16:40:27 +0530

    build issue resolved

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import './index.css'; // Import the CSS file
 import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
